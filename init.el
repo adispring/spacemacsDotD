9640963b94d86d94a629411bbe76d3228f3f51e5
王增迪 <sunnyadi@163.com>
2017-11-09 23:23:54 +0800

    move user-config to adispring-config

--- init.el
+++ init.el
@@ -356,97 +356,14 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
-  ;;============= adi's config =======================
-
-  ;; (push "~/.spacemacs.d" load-path)
-
   ;; fix tramp-method
-  (require 'tramp)
+  ;; (require 'tramp)
 
   ;; mode global enable
-  ;; (global-auto-complete-mode t)
   (global-company-mode t)
-  (electric-pair-mode t)
-  ;; https://www.reddit.com/r/emacs/comments/4xhxfw/how_to_tune_the_behavior_of_eletricpairmode/
-  (setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
-  (setq electric-pair-pairs '( (?\" . ?\") (?\` . ?\`) (?\( . ?\)) (?\{ . ?\}) (?\' . ?\') ))
-  (show-paren-mode t)
 
   ;; ivy swiper counsel mode
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-
-  ;; haskell setting
-  (setq-default
-   ;; hoogle in emacs instead of browser
-   haskell-hoogle-command t
-   )
-
-  ;; web setting
-  (add-to-list 'auto-mode-alist '("\\.erb\\'"    . web-mode))       ;; ERB
-  (add-to-list 'auto-mode-alist '("\\.html?\\'"  . web-mode))       ;; Plain HTML
-  (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))       ;; JS + JSX
-  (add-to-list 'auto-mode-alist '("\\.es6\\'"    . web-mode))       ;; ES6
-  (add-to-list 'auto-mode-alist '("\\.css\\'"    . web-mode))       ;; CSS
-  (add-to-list 'auto-mode-alist '("\\.scss\\'"   . web-mode))       ;; SCSS
-  (add-to-list 'auto-mode-alist '("\\.php\\'"   . web-mode))        ;; PHP
-  (add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))  ;; Blade template
-  (setq web-mode-content-types-alist
-        '(("jsx" . "\\.js[x]?\\'")
-          ("javascript" . "\\.es6?\\'")))
-
-  (setq web-mode-engines-alist
-        '(("blade"  . "\\.blade\\.")))
-
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "jsx")
-        (let ((web-mode-enable-part-face nil))
-          ad-do-it)
-      ad-do-it))
-
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "js")
-        (let ((web-mode-enable-part-face nil))
-          ad-do-it)
-      ad-do-it))
-
-  (setq web-mode-enable-auto-pairing t)
-
-  (setq web-mode-enable-css-colorization t)
-
-  ;; disable jshint since we prefer eslint checking
-  (with-eval-after-load 'flycheck
-    (setq-default flycheck-disabled-checkers
-                  (append flycheck-disabled-checkers
-                        '(javascript-jshint)))
-    ;; use eslint with web-mode for jsx files
-    (flycheck-add-mode 'javascript-eslint 'web-mode))
-
-  ;; M . : jump to definitions
-  (setq ac-js2-evaluate-calls t)
-
-  (add-hook 'web-mode-hook (lambda () (tern-mode t)))
-  ;; (eval-after-load 'tern
-  ;;   '(progn
-  ;;      (require 'tern-auto-complete)
-  ;;      (tern-ac-setup)))
-
-  (with-eval-after-load "web-mode"
-    (web-mode-toggle-current-element-highlight)
-    (web-mode-dom-errors-show))
-
-  (setq company-backends-web-mode '((company-tern ;; auto display js module apis
-                                     ;; company-dabbrev-code
-                                     ;; company-keywords
-                                     ;; company-etags
-                                     )
-                                    company-files
-                                    ;; company-dabbrev
-                                    ))
+  (ivy-mode t)
 
   ;; disable company default <return> behavior
   ;;; Prevent suggestions from being triggered automatically. In particular,
@@ -483,11 +400,6 @@ you should place your code here."
   ;; Company appears to override the above keymap based on company-auto-complete-chars.
   ;; Turning it off ensures we have full control.
   (setq company-auto-complete-chars nil)
-
-  (setq org-agenda-files (list "~/org/work.org"
-                               "~/org/learn.org"))
-
-  ;; (add-hook 'edit-server-done-hook (lambda () (shell-command "open -a \"Google Chrome\"")))
 
   )
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
