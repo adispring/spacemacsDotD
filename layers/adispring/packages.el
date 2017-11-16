c138032692729bb48f3148248c3d6c83fb2964d6
王增迪 <sunnyadi@163.com>
2017-11-16 08:35:59 +0800

    add dired-mode init config, but still can not bind C-p to dired-previous-line

--- layers/adispring/packages.el
+++ layers/adispring/packages.el
@@ -31,6 +31,7 @@
 
 (defconst adispring-packages
   '(
+    (dired-mode :location built-in)
     youdao-dictionary
     flycheck
     web-mode
@@ -39,6 +40,69 @@
     ac-js2
     )
   )
+(defun adispring/init-dired-mode ()
+  (use-package dired-mode
+    :defer t
+    :init
+    (progn
+      (require 'dired-x)
+      (require 'dired-aux)
+      (setq dired-listing-switches "-alh")
+      (setq dired-guess-shell-alist-user
+            '(("\\.pdf\\'" "open")
+              ("\\.docx\\'" "open")
+              ("\\.\\(?:djvu\\|eps\\)\\'" "open")
+              ("\\.\\(?:jpg\\|jpeg\\|png\\|gif\\|xpm\\)\\'" "open")
+              ("\\.\\(?:xcf\\)\\'" "open")
+              ("\\.csv\\'" "open")
+              ("\\.tex\\'" "open")
+              ("\\.\\(?:mp4\\|mkv\\|avi\\|flv\\|ogv\\)\\(?:\\.part\\)?\\'"
+               "open")
+              ("\\.\\(?:mp3\\|flac\\)\\'" "open")
+              ("\\.html?\\'" "open")
+              ("\\.md\\'" "open")))
+
+      (setq dired-omit-files
+            (concat dired-omit-files "\\|^.DS_Store$\\|^.projectile$\\|\\.js\\.meta$\\|\\.meta$"))
+
+      ;; always delete and copy recursively
+      (setq dired-recursive-deletes 'always)
+      (setq dired-recursive-copies 'always)
+
+      (defun ora-ediff-files ()
+        (interactive)
+        (let ((files (dired-get-marked-files))
+              (wnd (current-window-configuration)))
+          (if (<= (length files) 2)
+              (let ((file1 (car files))
+                    (file2 (if (cdr files)
+                               (cadr files)
+                             (read-file-name
+                              "file: "
+                              (dired-dwim-target-directory)))))
+                (if (file-newer-than-file-p file1 file2)
+                    (ediff-files file2 file1)
+                  (ediff-files file1 file2))
+                (add-hook 'ediff-after-quit-hook-internal
+                          (lambda ()
+                            (setq ediff-after-quit-hook-internal nil)
+                            (set-window-configuration wnd))))
+            (error "no more than 2 files should be marked"))))
+
+      (define-key dired-mode-map "e" 'ora-ediff-files)
+      ;; TODO: have no effect.
+      (define-key dired-mode-map (kbd "C-p") 'dired-previous-line)
+
+      (defvar dired-filelist-cmd
+        '(("vlc" "-L")))
+
+      )))
+
+;; TODO: have no effect.
+(defun adispring/post-init-dired-mode ()
+  (with-eval-after-load 'dired-mode
+    (define-key dired-mode-map (kbd "C-p") 'dired-previous-line)
+    ))
 
 (defun adispring/init-youdao-dictionary ()
   (use-package youdao-dictionary
