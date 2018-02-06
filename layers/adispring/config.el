1c92142937f98dbfb0f6d612ba47a803c6afe148
王增迪 <sunnyadi@163.com>
2018-02-06 11:53:27 +0800

    +  (setq js-indent-level 2) +  (setq css-indent-offset  2)

--- layers/adispring/config.el
+++ layers/adispring/config.el
@@ -31,3 +31,14 @@
     (beginning-of-line)
     (if (looking-at-p "^ +\/?> *$")
         (delete-char sgml-basic-offset))))
+
+(defun spacemacs/check-large-file ()
+  (when (> (buffer-size) 500000)
+    (progn (fundamental-mode)
+           (hl-line-mode -1)))
+  (if (and (executable-find "wc")
+           (> (string-to-number (shell-command-to-string (format "wc -l %s" (buffer-file-name))))
+              5000))
+      (linum-mode -1)))
+
+(add-hook 'find-file-hook 'spacemacs/check-large-file)
--- layers/adispring/funcs.el
+++ layers/adispring/funcs.el
@@ -44,6 +44,9 @@
   (call-interactively 'occur))
 
 (defun adi-web-mode-indent-setup ()
+  ;; web development
+  (setq js-indent-level 2)
+  (setq css-indent-offset  2)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
