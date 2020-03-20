c66f0c0200f21c091770149fbd409eccfff93806
adispring <sunnyadi@163.com>
2020-03-20 18:01:05 +0800

    fix typo: counsel-rg -> counsel-ag

--- layers/adispring/keybindings.el
+++ layers/adispring/keybindings.el
@@ -15,7 +15,7 @@
 ;; Ivy-based interface to shell and system tools
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-rg)
+(global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-c m") 'counsel-linux-app)
 (global-set-key (kbd "C-c n") 'counsel-fzf)
 (global-set-key (kbd "C-x l") 'counsel-locate)
